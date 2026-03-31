f619b4bd85d56142efc3b8cfd937fe23f92c8cf3
Cruelasss <irula5198@gmail.com>
2026-03-31 12:06:22 +0700

    refactor: menggunakan fungsi rata-rata dan status_lulus untuk menentukan kelulusan serta menghapus kode manual

--- elself.cpp
+++ elself.cpp
@@ -6,10 +6,33 @@ float Rata_rata (float a, float b) {
 
 }
 
-string status (float r ){
+string status_lulus (float r ){
     if (r >= 60) {
         return "Lulus";
     } else {
         return "Gagal";
     }
+}
+
+int main () {
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout << "Masukkan Nilai Bahasa Indonesia : ";
+    cin >> nilBI;
+    cout << "Masukkan Nilai Matematika : ";
+    cin >> nilMT;
+ 
+    //rerata = (nilBI + nilMT) / 2;
+    //if (rerata >= 60) {
+    //    status = "Lulus";
+    //} else {
+      //  status = "Gagal";
+    //}
+
+    rerata = Rata_rata(nilBI,nilMT);
+
+   // status = status_lulus(rerata);
+
+    cout << "Status Kelulusan : " <<status_lulus(rerata) <<" , dengan nilai rerata = "<<rerata<< endl;
 }

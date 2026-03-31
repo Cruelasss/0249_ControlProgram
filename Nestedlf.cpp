b5b3f352b2fc341ce41eb9030a103c589161d84e
Cruelasss <irula5198@gmail.com>
2026-03-31 12:09:41 +0700

    refactor: memperbarui pemanggilan fungsi Status_lulus pada main untuk menampilkan kategori kelulusan

--- Nestedlf.cpp
+++ Nestedlf.cpp
@@ -17,3 +17,26 @@ string Status_lulus (float r ){
         return "Gagal";
     }
 }
+
+int main () {
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout << "Masukkan Nilai Bahasa Indonesia : ";
+    cin >> nilBI;
+    cout << "Masukkan Nilai Matematika : ";
+    cin >> nilMT;
+ 
+    //rerata = (nilBI + nilMT) / 2;
+    //if (rerata >= 60) {
+    //    status = "Lulus";
+    //} else {
+      //  status = "Gagal";
+    //}
+
+    rerata = Rata_rata(nilBI,nilMT);
+
+   // status = status_lulus(rerata);
+
+    cout << "Status Kelulusan : " <<Status_lulus(rerata) <<" , dengan nilai rerata = "<<rerata<< endl;
+}
